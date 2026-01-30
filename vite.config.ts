e1231e3aca77392927abf7580ec80aa1ff4ea987
butter <butterflyjin77@gmail.com>
2026-01-30 15:00:00 +0000

    클라우드플레어 터널 뚫기 완료

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
+// vite.config.js
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react' // 사용하는 프레임워크에 따라 다름
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // 외부 접속 허용
+    port: 5173,
+    allowedHosts: [
+      'butterflyjin.kr',
+      'www.butterflyjin.kr' // 필요한 경우 추가
+    ]
+  }
 })
